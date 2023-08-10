e55136d89c69973869bd6b879c221e2960430a5b
Ricardo López <ricardo.lopez@b-process.mx>
2023-08-09 23:46:43 -0600

    Ajustando los problemas de roles

--- src/main/java/com/debuggeandoideas/app_security/Controllers/WelcomeController.java
+++ src/main/java/com/debuggeandoideas/app_security/Controllers/WelcomeController.java
@@ -3,10 +3,12 @@ package com.debuggeandoideas.app_security.Controllers;
 import java.util.Collections;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@CrossOrigin(origins = "http://localhost:4200")
 public class WelcomeController {
 
 	@GetMapping("/welcome")
--- src/main/java/com/debuggeandoideas/app_security/Security/SecurityConfig.java
+++ src/main/java/com/debuggeandoideas/app_security/Security/SecurityConfig.java
@@ -1,9 +1,13 @@
 package com.debuggeandoideas.app_security.Security;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +15,12 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -30,11 +40,28 @@ public class SecurityConfig {
 	
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+		var requestHandler = new CsrfTokenRequestAttributeHandler();
+		requestHandler.setCsrfRequestAttributeName("_csrf");
 		http.authorizeHttpRequests(auth ->
-			auth.requestMatchers("/loans","/balance","/account","/cards").hasAnyRole("ADMINISTRADOR","DESARROLLADOR","CONSULTOR")//para todos es authenticated
+			//auth.requestMatchers("/loans","/balance","/account","/cards").hasAnyRole("ADMINISTRADOR","DESARROLLADOR","CONSULTOR")//para todos es authenticated
+			auth.requestMatchers("/loans").hasAuthority("ADMINISTRADOR")
+				.requestMatchers("/balance").hasAuthority("DESARROLLADOR")
+				.requestMatchers("/accounts").hasAuthority("CONSULTOR")
+				.requestMatchers("/cards").hasAnyAuthority("CONSULTOR","DESARROLLADOR")
 				.requestMatchers("/welcome","about").permitAll())//tambien pudo haber quedado como .anyRequest().permitAll())
 			.formLogin(Customizer.withDefaults())
-			.httpBasic(Customizer.withDefaults());
+			.httpBasic(Customizer.withDefaults())
+			.cors(cors -> cors.configurationSource(corseConfigurationsSource())) // Aplica la configuración CORS
+			.csrf(csrf -> csrf
+					.csrfTokenRequestHandler(requestHandler)
+					.ignoringRequestMatchers("/welcome","/about")
+					.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+			.addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class);
+		/*Para eliminar errores del CORS*/
+		//.cors(cors-> corseConfigurationsSource())
+		//.csrf().disable();
+		//http.csrf(AbstractHttpConfigurer::disable);
+		//http.cors(cors-> corseConfigurationsSource());
 		return http.build();
 		
 	}
@@ -61,4 +88,22 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 	
+	@Bean
+	CorsConfigurationSource corseConfigurationsSource() {
+		var config = new CorsConfiguration();
+		//config.setAllowedOrigins(List.of("http://localhost:4200"));
+		//config.setAllowCredentials(true);
+		//config.setAllowedOriginPatterns(List.of("http://localhost:4200")); // Cambia "allowedOrigins" a "allowedOriginPatterns"
+		config.setAllowedOrigins(List.of("*"));
+		//config.setAllowedMethods(List.of("GET","POST","GET","DELETE"));
+		config.setAllowedMethods(List.of("*"));
+		config.setAllowedHeaders(List.of("*"));
+		//config.setAllowedOrigins(List.of("*"));
+		//config.setAllowCredentials(true);
+		//config.setAllowedHeaders(List.of("Access-Control-Allow-Headers", "Access-Control-Allow-Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin", "Cache-Control", "Content-Type", "Authorization"));
+		//config.setAllowedMethods(List.of("DELETE", "GET", "POST", "PATCH", "PUT"));
+		var source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return source;
+	}
 }
